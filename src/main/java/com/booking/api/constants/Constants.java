7b98ae402ae3d8f35534ca43f34fb47b36d46468
Jean Xavier <xxajean@rakbank.ae>
2023-09-26 16:59:12 +0400

    feat: retrieve patient with id closes #4

--- src/main/java/com/booking/api/constants/Constants.java
+++ src/main/java/com/booking/api/constants/Constants.java
@@ -3,6 +3,8 @@ package com.booking.api.constants;
 public final class Constants {
     public static final String PATIENT_CREATED_SUCCESSFULLY = "Patient created successfully";
     public static final String PATIENT_ALREADY_EXITS = "Patient already exists";
+    public static final String PATIENT_NOT_FOUND = "No Patient Found";
+    public static final String PATIENT_RETRIEVED = "Success";
 
     private Constants() {
     }
